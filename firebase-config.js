a16aaae9a7332ebf4caa3e891aa91c8dd80cd23b
adfirth <adfirth@gmail.com>
2025-06-13 21:27:28 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzBG-4nWnF7WoKP-pCvJ_9ewL_gCK94W0",
+  authDomain: "gp-triage-dashboard.firebaseapp.com",
+  projectId: "gp-triage-dashboard",
+  storageBucket: "gp-triage-dashboard.firebasestorage.app",
+  messagingSenderId: "901162590030",
+  appId: "1:901162590030:web:05c2d51de894a0ab31c81f",
+  measurementId: "G-11NKQDGXT0"
+};
